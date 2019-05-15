0073be1b9a7da0280473b4c1270670cd39806ab1
monidl <36744918+monidl@users.noreply.github.com>
2019-05-15 15:28:42 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,34 +1,24 @@
 // "a|n|a".length /2 = parseInt(1.5) = 1
 // "ab||ba".length /2 = parseInt(2) = 2
+const word = "abba";
 
-const word = "abba"
-isPalindrome(word);
+function isPalindrome(word) {
+  var enMedio = parseInt(word.length / 2) - 1; // 5
+  var size = word.length;
+  let result = true;
 
-function isPalindrome (word){ //"Lamina animal"
-    //par o ompar
-    //            parseInt((13          /2)) = 6
-    var enMedio = parseInt((word.length/2)) - 1;// 5
-    var size = word.length;
-    let result = true;
-
-    for (var i=0; i <= enMedio; i++){ //i=0
-      if ( 
-          word[i].charCodeAt(0) === word [size - (i+1).charCodeAt]
-       || word[i].charCodeAt(0) + 32 === word [size - (i+1).charCodeAt]
-       || word[i].charCodeAt(0) === word [size - (i+1).charCodeAt] + 32
-      ){
-        result = true;
-       
-      }else{
-      
-        result = false;
-        break;
-      }
+  for (var i = 0; i <= enMedio; i++) {
+    //i=0
+    if (
+      word[i].charCodeAt(0) === word[size - (i + 1)].charCodeAt(0) ||
+      word[i].charCodeAt(0) + 32 === word[size - (i + 1)].charCodeAt(0) ||
+      word[i].charCodeAt(0) === word[size - (i + 1)].charCodeAt(0) + 32
+    ) {
+      return true;
+    } else {
+      return false;
     }
-    
-  console.log(result);
+  }
 }
 
-
-
-
+console.log("Is palindrome: " + isPalindrome(word));
